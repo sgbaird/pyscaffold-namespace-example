ed06ed4fef12b89a9d019a6fdf6119bbb425ddd3
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-16 16:55:24 -0400

    sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -153,15 +153,12 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
-}
+html_theme_options = {"sidebar_width": "300px", "page_width": "1200px"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -246,7 +243,13 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "pyscaffold-namespace-example Documentation", "Sterling G. Baird", "manual")
+    (
+        "index",
+        "user_guide.tex",
+        "pyscaffold-namespace-example Documentation",
+        "Sterling G. Baird",
+        "manual",
+    )
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
